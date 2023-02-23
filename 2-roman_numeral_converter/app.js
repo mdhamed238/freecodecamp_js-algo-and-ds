2b2bd33a7b51e435be04e8d3b90a573a654edc09
Mohamed Hamed <mdhamed0238@gmail.com>
2023-02-23 19:20:02 +0000

    [Task] Roman numeral converter

---
+++ 2-roman_numeral_converter/app.js
@@ -0,0 +1,28 @@
+function convertToRoman(num) {
+	let result = '';
+	const romanNumerals = {
+		M: 1000,
+		CM: 900,
+		D: 500,
+		CD: 400,
+		C: 100,
+		XC: 90,
+		L: 50,
+		XL: 40,
+		X: 10,
+		IX: 9,
+		V: 5,
+		IV: 4,
+		I: 1,
+	};
+
+	for (let key in romanNumerals) {
+		while (num >= romanNumerals[key]) {
+			result += key;
+			num -= romanNumerals[key];
+		}
+	}
+	return result;
+}
+
+console.log(convertToRoman(36));
